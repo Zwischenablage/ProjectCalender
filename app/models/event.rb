d01c84a5fbc04097a8b22ff8088fe6807ce59832
Zwischenablage <github@zwischenablage.org>
2018-10-24 15:05:04 +0200

    added validations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :project
+  validates :event_type, presence: true
+  validates :event_description, presence: true
+  validates :event_start, presence: true
+  validates :event_duration, presence: true
   enum event_type: [ :Porting, :APQM, :Tuning ]
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
-    has_many :events
-    #validates :title, presence: true, length: { minimum: 5 }
+    has_many :events, dependent: :destroy
+    validates :project_name, presence: true
+    validates :project_tier1, presence: true
+    validates :project_oem, presence: true
 end
